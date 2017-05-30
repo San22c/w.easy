394b3aa4698fe66e4bb89b12b3f887ae885f966e
San22c <cartas22c@gmail.com>
2017-05-30 20:24:06 +0200

    fixed psql

--- app/helpers/resultados_helper.rb
+++ app/helpers/resultados_helper.rb
@@ -2,8 +2,8 @@ module ResultadosHelper
 
   def valoracion
      puntuacion = []
-    resultados_bad = Resultado.where(:fichero_id => session[:fichero_id], :codError => 1).group(:estandar_id)
-    resultados_ok  = Resultado.where(:fichero_id => session[:fichero_id], :codError => 0).group(:estandar_id)
+    resultados_bad = Resultado.select(:estandar_id).where(:fichero_id => session[:fichero_id], :codError => 1).group(:estandar_id)
+    resultados_ok  = Resultado.select(:estandar_id).where(:fichero_id => session[:fichero_id], :codError => 0).group(:estandar_id)
     estandar_bad   = resultados_bad.map {|bad| bad.estandar_id }
 
     resultados_ok = Resultado.where('fichero_id = ? AND  estandar_id NOT IN (?) AND codError = 0',session[:fichero_id],estandar_bad).group(:estandar_id)
@@ -11,8 +11,7 @@ module ResultadosHelper
 
     puntuacion.push(estandar_ok.size)
     puntuacion.push(estandar_bad.size)
-    byebug
-     return puntuacion
+      return puntuacion
   end
 
 end
